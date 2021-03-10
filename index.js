7db25833582052daabcfdac9db94693459b80489
SeungsooChoi <34928445+SeungsooChoi@users.noreply.github.com>
2021-03-10 18:12:33 +0900

    test get method + install nodemon, babel/node

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const express = require("express"); // require : express라는 폴더를 내 파일들 중 찾고 없으면 node module에서 찾음
+// const express = require("express"); // require : express라는 폴더를 내 파일들 중 찾고 없으면 node module에서 찾음
+import express from "express";
 const app = express();
 
 const PORT = 4000;
 
-const handleListening = () => {
+const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);
-};
+
+const handleHome = (req, res) => res.send("Hello World");
+
+app.get("/", handleHome);
 
 app.listen(4000, handleListening);
