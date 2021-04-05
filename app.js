8ac108df1449a935e3afeb70c2c5e08619569283
SeungsooChoi <34928445+SeungsooChoi@users.noreply.github.com>
2021-04-05 22:01:08 +0900

    install multer and show videos in home

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const app = express();
 // middleware
 app.use(helmet()); // help secure
 app.set("view engine", "pug");
+app.use("/uploads", express.static("uploads")); // directory에서 file을 보내주는 middleware
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- middlewares.js
+++ middlewares.js
@@ -1,5 +1,8 @@
 // 이 파일을 통해서 siteName, routes를 pug 파일에서 변수처럼 사용할 수 있다.
+import multer from "multer";
 import routes from "./routes";
+
+const multerVideo = multer({ dest: "uploads/videos/" });
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "WeTube";
@@ -11,3 +14,5 @@ export const localsMiddleware = (req, res, next) => {
   };
   next();
 };
+
+export const uploadVideo = multerVideo.single("videoFile");
